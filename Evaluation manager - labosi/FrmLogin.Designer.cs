a8bbb968d67c68f9eb402f38e9dc53007b236c5f
David Matijanić <dmatijani21@foi.hr>
2023-05-14 15:37:46 +0200

    Implementirana provjera prijave korisnika

--- Evaluation manager - labosi/FrmLogin.Designer.cs
+++ Evaluation manager - labosi/FrmLogin.Designer.cs
@@ -78,6 +78,7 @@
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "Prijava";
             this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // FrmLogin
             // 
--- Evaluation manager - labosi/FrmLogin.cs
+++ Evaluation manager - labosi/FrmLogin.cs
@@ -12,11 +12,15 @@ namespace Evaluation_manager___labosi
 {
     public partial class FrmLogin : Form
     {
+        // Hard-codani korisničko ime i lozinka
+        string username = "nastavnik";
+        string password = "test";
+
         public FrmLogin()
         {
             InitializeComponent();
         }
-
+        
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -25,6 +29,48 @@ namespace Evaluation_manager___labosi
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
+        }
+        
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            int usernamePasswordCheck = 0;
+            if (txtUsername.Text != "") usernamePasswordCheck += 2;
+            if (txtPassword.Text != "") usernamePasswordCheck++;
+
+            // Provjerava je li sve uneseno
+            switch (usernamePasswordCheck)
+            {
+                case 0:
+                {
+                    MessageBox.Show("Korisničko ime i lozinka nisu uneseni!", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                break;
+
+                case 1:
+                {
+                    MessageBox.Show("Korisničko ime nije uneseno!", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                break;
+
+                case 2:
+                {
+                    MessageBox.Show("Lozinka nije unesena!", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                break;
+
+                case 3:
+                {
+                    if ((txtUsername.Text == username) && (txtPassword.Text == password))
+                    {
+                        MessageBox.Show("Sve je ispravno uneseno!", "Uspješna prijava", MessageBoxButtons.OK);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Neispravni podaci!", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                break;
+            }
         }
     }
 }
