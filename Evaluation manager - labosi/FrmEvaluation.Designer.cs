b754e6e3f63be1a5065af163f5f1afff228bf412
David Matijanić <dmatijani21@foi.hr>
2023-05-14 18:33:28 +0200

    Dodana forma za evaluaciju

---
+++ Evaluation manager - labosi/FrmEvaluation.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Evaluation_manager___labosi
+{
+    partial class FrmEvaluation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboActivities = new System.Windows.Forms.ComboBox();
+            this.lblActivities = new System.Windows.Forms.Label();
+            this.txtMinForSignature = new System.Windows.Forms.TextBox();
+            this.txtMinForGrade = new System.Windows.Forms.TextBox();
+            this.lblMinForSignature = new System.Windows.Forms.Label();
+            this.lblMinForGrade = new System.Windows.Forms.Label();
+            this.txtActivityDescription = new System.Windows.Forms.TextBox();
+            this.txtTeacher = new System.Windows.Forms.TextBox();
+            this.txtEvaluationDate = new System.Windows.Forms.TextBox();
+            this.numPoints = new System.Windows.Forms.NumericUpDown();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblTeacher = new System.Windows.Forms.Label();
+            this.lblEvaluationDate = new System.Windows.Forms.Label();
+            this.lblPoints = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numPoints)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboActivities
+            // 
+            this.cboActivities.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboActivities.FormattingEnabled = true;
+            this.cboActivities.Location = new System.Drawing.Point(12, 27);
+            this.cboActivities.Name = "cboActivities";
+            this.cboActivities.Size = new System.Drawing.Size(243, 21);
+            this.cboActivities.TabIndex = 0;
+            // 
+            // lblActivities
+            // 
+            this.lblActivities.AutoSize = true;
+            this.lblActivities.Location = new System.Drawing.Point(12, 11);
+            this.lblActivities.Name = "lblActivities";
+            this.lblActivities.Size = new System.Drawing.Size(54, 13);
+            this.lblActivities.TabIndex = 1;
+            this.lblActivities.Text = "Aktivnost:";
+            // 
+            // txtMinForSignature
+            // 
+            this.txtMinForSignature.Location = new System.Drawing.Point(293, 27);
+            this.txtMinForSignature.Name = "txtMinForSignature";
+            this.txtMinForSignature.ReadOnly = true;
+            this.txtMinForSignature.Size = new System.Drawing.Size(132, 20);
+            this.txtMinForSignature.TabIndex = 2;
+            // 
+            // txtMinForGrade
+            // 
+            this.txtMinForGrade.Location = new System.Drawing.Point(431, 27);
+            this.txtMinForGrade.Name = "txtMinForGrade";
+            this.txtMinForGrade.ReadOnly = true;
+            this.txtMinForGrade.Size = new System.Drawing.Size(132, 20);
+            this.txtMinForGrade.TabIndex = 3;
+            // 
+            // lblMinForSignature
+            // 
+            this.lblMinForSignature.AutoSize = true;
+            this.lblMinForSignature.Location = new System.Drawing.Point(290, 11);
+            this.lblMinForSignature.Name = "lblMinForSignature";
+            this.lblMinForSignature.Size = new System.Drawing.Size(80, 13);
+            this.lblMinForSignature.TabIndex = 4;
+            this.lblMinForSignature.Text = "Uvjet za potpis:";
+            // 
+            // lblMinForGrade
+            // 
+            this.lblMinForGrade.AutoSize = true;
+            this.lblMinForGrade.Location = new System.Drawing.Point(428, 11);
+            this.lblMinForGrade.Name = "lblMinForGrade";
+            this.lblMinForGrade.Size = new System.Drawing.Size(84, 13);
+            this.lblMinForGrade.TabIndex = 5;
+            this.lblMinForGrade.Text = "Uvjet za ocjenu:";
+            this.lblMinForGrade.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtActivityDescription
+            // 
+            this.txtActivityDescription.Location = new System.Drawing.Point(12, 62);
+            this.txtActivityDescription.Multiline = true;
+            this.txtActivityDescription.Name = "txtActivityDescription";
+            this.txtActivityDescription.ReadOnly = true;
+            this.txtActivityDescription.Size = new System.Drawing.Size(551, 107);
+            this.txtActivityDescription.TabIndex = 6;
+            // 
+            // txtTeacher
+            // 
+            this.txtTeacher.Location = new System.Drawing.Point(282, 213);
+            this.txtTeacher.Name = "txtTeacher";
+            this.txtTeacher.ReadOnly = true;
+            this.txtTeacher.Size = new System.Drawing.Size(108, 20);
+            this.txtTeacher.TabIndex = 7;
+            // 
+            // txtEvaluationDate
+            // 
+            this.txtEvaluationDate.Location = new System.Drawing.Point(396, 213);
+            this.txtEvaluationDate.Name = "txtEvaluationDate";
+            this.txtEvaluationDate.ReadOnly = true;
+            this.txtEvaluationDate.Size = new System.Drawing.Size(108, 20);
+            this.txtEvaluationDate.TabIndex = 8;
+            // 
+            // numPoints
+            // 
+            this.numPoints.Location = new System.Drawing.Point(510, 213);
+            this.numPoints.Name = "numPoints";
+            this.numPoints.Size = new System.Drawing.Size(53, 20);
+            this.numPoints.TabIndex = 9;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(452, 251);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(111, 23);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "Odustani";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(335, 251);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(111, 23);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Spremi";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // lblTeacher
+            // 
+            this.lblTeacher.AutoSize = true;
+            this.lblTeacher.Location = new System.Drawing.Point(279, 197);
+            this.lblTeacher.Name = "lblTeacher";
+            this.lblTeacher.Size = new System.Drawing.Size(60, 13);
+            this.lblTeacher.TabIndex = 12;
+            this.lblTeacher.Text = "Ocjenjivač:";
+            // 
+            // lblEvaluationDate
+            // 
+            this.lblEvaluationDate.AutoSize = true;
+            this.lblEvaluationDate.Location = new System.Drawing.Point(393, 197);
+            this.lblEvaluationDate.Name = "lblEvaluationDate";
+            this.lblEvaluationDate.Size = new System.Drawing.Size(92, 13);
+            this.lblEvaluationDate.TabIndex = 13;
+            this.lblEvaluationDate.Text = "Datum evaluacije:";
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.Location = new System.Drawing.Point(507, 197);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(43, 13);
+            this.lblPoints.TabIndex = 14;
+            this.lblPoints.Text = "Bodovi:";
+            // 
+            // FrmEvaluation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 286);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.lblEvaluationDate);
+            this.Controls.Add(this.lblTeacher);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.numPoints);
+            this.Controls.Add(this.txtEvaluationDate);
+            this.Controls.Add(this.txtTeacher);
+            this.Controls.Add(this.txtActivityDescription);
+            this.Controls.Add(this.lblMinForGrade);
+            this.Controls.Add(this.lblMinForSignature);
+            this.Controls.Add(this.txtMinForGrade);
+            this.Controls.Add(this.txtMinForSignature);
+            this.Controls.Add(this.lblActivities);
+            this.Controls.Add(this.cboActivities);
+            this.Name = "FrmEvaluation";
+            this.Text = "FrmEvaluation";
+            ((System.ComponentModel.ISupportInitialize)(this.numPoints)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboActivities;
+        private System.Windows.Forms.Label lblActivities;
+        private System.Windows.Forms.TextBox txtMinForSignature;
+        private System.Windows.Forms.TextBox txtMinForGrade;
+        private System.Windows.Forms.Label lblMinForSignature;
+        private System.Windows.Forms.Label lblMinForGrade;
+        private System.Windows.Forms.TextBox txtActivityDescription;
+        private System.Windows.Forms.TextBox txtTeacher;
+        private System.Windows.Forms.TextBox txtEvaluationDate;
+        private System.Windows.Forms.NumericUpDown numPoints;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblTeacher;
+        private System.Windows.Forms.Label lblEvaluationDate;
+        private System.Windows.Forms.Label lblPoints;
+    }
+}
---
+++ Evaluation manager - labosi/FrmEvaluation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluation_manager___labosi
+{
+    public partial class FrmEvaluation : Form
+    {
+        public FrmEvaluation()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
